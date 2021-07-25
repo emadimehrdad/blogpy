f7c266c4b665e75c409f048baec0a08e6a03e809
mehrdad <emadi_mehrdad@yahoo.com>
2021-07-25 21:25:42 +0430

    add index.html to blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+from . import *
+
+
+class IndexPage(TemplateView):
+    template_name = 'index.html'
